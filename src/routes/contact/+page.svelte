4bf9b49cffb1530240d6b41445249a4399d0a82b
Aditya <52035504+smashfries@users.noreply.github.com>
2025-06-27 10:23:20 +0530

    Integrated contact form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -4,6 +4,44 @@
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
+	let name = $state('');
+	let email = $state('');
+	let organization = $state('');
+	let message = $state('');
+
+	let messageSent = $state(false);
+
+	async function sendMessage(event) {
+		event.preventDefault();
+
+		const res = await fetch('https://api.devmatter.app/forms/1', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer YOUR_API_KEY'
+			},
+			body: JSON.stringify({
+				'full-name': name,
+				email,
+				organization,
+				message
+			})
+		});
+
+		if (res.ok) {
+			messageSent = true;
+			resetForm();
+		} else {
+			alert('Failed to send message');
+		}
+	}
+
+	function resetForm() {
+		name = '';
+		email = '';
+		organization = '';
+		message = '';
+	}
 </script>
 
 <Header user={data.user} />
@@ -18,52 +56,71 @@
 			</p>
 		</div>
 		<div class="col-span-2 sm:col-span-1">
-			<form class="flex flex-col gap-6">
-				<div class="flex flex-col gap-1">
-					<label for="name" class="text-sm font-medium text-zinc-600">Name*</label>
-					<input
-						type="text"
-						id="name"
-						name="name"
-						required
-						class="rounded-md border-zinc-300 focus-visible:border-blue-600"
-					/>
-				</div>
-				<div class="flex flex-col gap-1">
-					<label for="email" class="text-sm font-medium text-zinc-600">Email*</label>
-					<input
-						type="email"
-						id="email"
-						required
-						name="email"
-						class="rounded-md border-zinc-300 focus-visible:border-blue-600"
-					/>
-				</div>
-				<div class="flex flex-col gap-1">
-					<label for="org" class="text-sm font-medium text-zinc-600">Organization</label>
-					<input
-						type="text"
-						id="org"
-						name="org"
-						class="rounded-md border-zinc-300 focus-visible:border-blue-600"
-					/>
-				</div>
-				<div class="flex flex-col gap-1">
-					<label for="msg" class="text-sm font-medium text-zinc-600">Message*</label>
-					<textarea
-						id="msg"
-						name="msg"
-						rows="5"
-						class="rounded-md border-zinc-300 focus-visible:border-blue-600"
-					></textarea>
-				</div>
-				<div>
+			{#if !messageSent}
+				<form class="flex flex-col gap-6" onsubmit={sendMessage}>
+					<div class="flex flex-col gap-1">
+						<label for="name" class="text-sm font-medium text-zinc-600">Name*</label>
+						<input
+							type="text"
+							id="name"
+							name="name"
+							bind:value={name}
+							required
+							class="rounded-md border-zinc-300 focus-visible:border-blue-600"
+						/>
+					</div>
+					<div class="flex flex-col gap-1">
+						<label for="email" class="text-sm font-medium text-zinc-600">Email*</label>
+						<input
+							type="email"
+							id="email"
+							required
+							name="email"
+							bind:value={email}
+							class="rounded-md border-zinc-300 focus-visible:border-blue-600"
+						/>
+					</div>
+					<div class="flex flex-col gap-1">
+						<label for="org" class="text-sm font-medium text-zinc-600">Organization</label>
+						<input
+							type="text"
+							id="org"
+							name="org"
+							bind:value={organization}
+							class="rounded-md border-zinc-300 focus-visible:border-blue-600"
+						/>
+					</div>
+					<div class="flex flex-col gap-1">
+						<label for="msg" class="text-sm font-medium text-zinc-600">Message*</label>
+						<textarea
+							id="msg"
+							name="msg"
+							rows="5"
+							bind:value={message}
+							required
+							class="rounded-md border-zinc-300 focus-visible:border-blue-600"
+						></textarea>
+					</div>
+					<div>
+						<button
+							class="flex items-center gap-2 rounded-xl bg-blue-600 px-4 py-2 text-sm font-medium text-white transition hover:bg-blue-600/90"
+							><Send size={16} />Send message</button
+						>
+					</div>
+				</form>
+			{:else}
+				<div class="flex flex-col items-center">
+					<p class="text-center text-xl font-medium text-zinc-800">Thank you for your message!</p>
+					<p class="text-center text-zinc-500">We will get back to you as soon as possible.</p>
+
 					<button
-						class="flex items-center gap-2 rounded-xl bg-blue-600 px-4 py-2 text-sm font-medium text-white transition hover:bg-blue-600/90"
-						><Send size={16} />Send message</button
+						onclick={() => (messageSent = !messageSent)}
+						class="mt-10 flex items-center gap-2 rounded-xl bg-blue-600 px-4 py-2 text-sm font-medium text-white"
+					>
+						<Send size={16} />Send another message</button
 					>
 				</div>
-			</form>
+			{/if}
 		</div>
 	</div>
 </div>
