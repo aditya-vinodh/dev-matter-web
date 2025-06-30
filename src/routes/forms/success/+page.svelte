efe0a766568297b82d3cc2d55b41d23aeb008af5
Aditya <52035504+smashfries@users.noreply.github.com>
2025-06-30 21:05:37 +0530

    Created simple forms success page

---
+++ src/routes/forms/success/+page.svelte
@@ -0,0 +1,11 @@
+<div class="mt-20 flex flex-col gap-20 pb-32 text-center">
+	<p class="text-3xl font-semibold tracking-tight">Your response has been submitted!</p>
+
+	<div class="flex flex-col items-center gap-4">
+		<img src="/devmatter-rounded.png" alt="logo" class="size-10" />
+		<p>
+			Try <a class="font-medium text-blue-600" href="https://devmatter.app">DevMatter</a> to manage your
+			projects!
+		</p>
+	</div>
+</div>
